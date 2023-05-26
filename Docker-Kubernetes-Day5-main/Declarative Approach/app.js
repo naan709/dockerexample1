b7ea5ee0e27c262eb154dc73f9c52864e51c70be
naan709 <naan709@gmail.com>
2023-05-26 07:31:39 +0000

    Adding yaml changes

--- Docker-Kubernetes-Day5-main/Declarative Approach/app.js
+++ Docker-Kubernetes-Day5-main/Declarative Approach/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+var os = require("os");
+var hostname = os.hostname();
 
 const app = express();
 
 app.get('/', (req, res) => {
+  console.log("input request received");
   res.send(`
-    <h1>Hello from this NodeJS app!</h1>
+    <h1>Hello from this NodeJS app! in ${hostname}</h1>
     <p>Try sending a request to /error and see what happens</p>
   `);
 });
